b07ce1916de60da47cfc68f2952053e298b1e6ed
Aryan Tiwari <aryantiwari7762@gmail.com>
2023-05-20 01:48:58 +0530

    hero section added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,8 @@
 import logo from "./logo.jpg";
 import menu from "./menu.svg"
+import imhero from "./imhero.jpg";
 export {
   logo,
   menu,
+  imhero
 };
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import {imhero} from "../assets"
 export const Hero = () => {
   return (
     <div className='w-full  px-16 py-10 flex justify-between'>
-        <div className='w-1/2 ml-10 flex flex-col gap-5'>
+        <div className=' py-20 w-1/2 ml-10 flex flex-col gap-5'>
             <h1 className='text-3xl font-bold'>
                 Virtual Health For You
             </h1>
@@ -11,12 +11,12 @@ export const Hero = () => {
                 just testing soemthing else will be types over here, lets just makes sure that it will exceed 2 lines so it will be better for testing
             </p>
             <div>
-            <button className='px-6 py-2  bg-blue-400  font-regular text-white rounded-lg'>
+            <button className='px-6 py-2  bg-blue-400 hover:bg-blue-500 font-regular text-white rounded-lg'>
                 Consult Today
             </button>
             </div>
         </div>
-        <img>
+        <img src={imhero.src} alt="gitu" className="w-[693px] h-[598px]" >
         </img>
     
     </div>
