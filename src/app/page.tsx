59ee9602ac7db3030c51d89cb6d6f91ea54d68f5
rupeshshandilya <rupeshkshandilya@gmail.com>
2023-05-20 00:12:12 +0530

    header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Header from "@/components/header";
+"use client"
+import { Header } from "@/components/header";
 
 export default function Home() {
   return (
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,58 @@
-import React from 'react'
-import { navLink } from '@/constants/constant'
-import { logo } from "../assets";
+import { navLink } from "@/constants/constant";
+import { useState } from "react";
+import {logo, menu} from "../assets"
 
-// Change Logo 
-{/* HOME,ABOUTUS,FEATURE,TESTIMONIAL,FAQ */}
-const Header = () => {
+
+export const Header = () => {
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false); 
+
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo.src} alt="apertus-logo" className='w-[90px] h-[90px]'/>
-        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-            {
-                navLink.map((nav, index) => (
-                    <li 
-                    key={nav.id}
-                    className={`font-normal cursor-pointer text-[16px]`}
-                    >
-                        <a>{nav.title}</a>
-                    </li>
-                ))
-            }
-        </ul>
-    </nav>
-  )
-}
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo.src} alt="hoobank" className="w-[60px] h-[60px]" />
 
-export default Header
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLink.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLink.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
+
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLink.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLink.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
