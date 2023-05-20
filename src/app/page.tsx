a39171b80554aaf5691ae4ec08873ef873befbf1
rupeshshandilya <rupeshkshandilya@gmail.com>
2023-05-20 17:14:52 +0530

    🎨 style(page.tsx): add missing line breaks and sort imports alphabetically The changes in this commit are purely cosmetic. The import statements were sorted alphabetically to improve readability and a few line breaks were added to separate the components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 "use client"
 
+import { AboutUs } from "@/components/aboutUs";
+import { FAQ } from "@/components/faq";
+import { Features } from "@/components/features";
+import Footer from "@/components/footer";
 import {Header} from "@/components/header";
 import { Hero } from "@/components/hero";
+import { Testimonial } from "@/components/testimonial";
 
 export default function Home() {
   return (
    <>
    <Header/>
    <Hero/>
+   <AboutUs/>
+   <Features/>
+   <Testimonial/>
+   <FAQ/>
+   <Footer/>
     </>
   )
 }
