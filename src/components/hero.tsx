3bbcf8a1beae1a408efd95e35427d3d83c76fc44
rupeshshandilya <rupeshkshandilya@gmail.com>
2023-05-20 17:13:53 +0530

    🎨 style(hero.tsx): refactor Hero component to improve readability and responsiveness The Hero component has been refactored to use a grid layout for better responsiveness. The section tag has been added to improve semantics. The text and button have been updated to use more modern styles. The image has been moved to the right side of the grid to improve the layout.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import {imhero} from "../assets"
 export const Hero = () => {
   return (
-    <div className='w-full  px-16 py-10 flex justify-between'>
-        <div className=' py-20 w-1/2 ml-10 flex flex-col gap-5'>
-            <h1 className='text-3xl font-bold'>
-                Virtual Health For You
-            </h1>
-            <p className='text-xl text-gray-500 font-regular'>
-                just testing soemthing else will be types over here, lets just makes sure that it will exceed 2 lines so it will be better for testing
-            </p>
-            <div>
-            <button className='px-6 py-2  bg-blue-400 hover:bg-blue-500 font-regular text-white rounded-lg'>
-                Consult Today
-            </button>
-            </div>
+    <section className="bg-white" id='hero'>
+    <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
+                Apertus Landing Page
+                </h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick
+                </p>
+            <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Get started
+                <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
         </div>
-        <img src={imhero.src} alt="gitu" className="w-[693px] h-[598px]" >
-        </img>
-    
+        <div className="lg:mt-0 lg:col-span-5 lg:flex">
+            <img src={imhero.src} alt="mockup" />
+        </div>                
     </div>
+</section>
   );
 };
