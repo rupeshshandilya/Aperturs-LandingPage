23e69541052a08bb38d4e69021f7040d036aa916
rupeshshandilya <rupeshkshandilya@gmail.com>
2023-05-20 17:13:28 +0530

    🎨 style(header.tsx): refactor Header component to use Material Tailwind components and improve responsiveness The Header component has been refactored to use Material Tailwind components, which improves the consistency of the UI. The component has also been made more responsive by adding support for mobile devices. The navigation menu has been moved to a separate component and is now collapsible on smaller screens. The logo has been made clickable and the code has been cleaned up.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,59 +1,127 @@
-import { navLink } from "@/constants/constant";
-import { useState } from "react";
-import {logo, menu, closeMenu} from "../assets"
-
-
-export const Header = () => {
-  const [active, setActive] = useState("Home");
-  const [toggle, setToggle] = useState(false); 
-
-  return (
-    <nav className="w-full flex py-6 px-10 justify-between items-center navbar">
-      <img src={logo.src} alt="hoobank" className="w-[60px] h-[60px]" />
-
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLink.map((nav, index) => (
-          <li
-            key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navLink.length - 1 ? "mr-0" : "mr-10"}`}
-            onClick={() => setActive(nav.title)}
-          >
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))}
-      </ul>
-
-
-      {/* For Mobile */}
-      <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img
-          src={toggle ? closeMenu.src : menu.src}
-          alt="menu-img"
-          className="w-[28px] h-[28px] object-contain"
-          onClick={() => setToggle(!toggle)}
-        />
-        <div
-          className={`${
-            !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-        >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
-            {navLink.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLink.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
+import React from "react";
+import { logo } from "@/assets";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+ 
+export function Header() {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+ 
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 text-gray-900">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#hero" className="flex items-center">
+          Home
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#about-us" className="flex items-center">
+          About Us
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#feature" className="flex items-center">
+          Features
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#testimonial" className="flex items-center">
+          Testimonoal
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#faq" className="flex items-center">
+          FAQ
+        </a>
+      </Typography>
+    </ul>
   );
-};
+ 
+  return (
+    <>
+      <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
+        <div className="flex items-center justify-between text-gray-900">
+          <img src={logo.src} className="mr-4 cursor-pointer py-1.5 w-[60px] h-[60px]"/>
+          <div className="flex items-center gap-4 text-gray-900">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+        <MobileNav open={openNav}>
+          {navList}
+        </MobileNav>
+      </Navbar>
+    </>
+  );
+}
