9932ea2a43b055bc2af6baae794f4dcabc51a280
rupeshshandilya <rupeshkshandilya@gmail.com>
2023-06-14 11:31:33 +0530

    content changed

--- src/components/aboutUs.tsx
+++ src/components/aboutUs.tsx
@@ -1,22 +1,47 @@
-import React from 'react'
-import { logo } from '@/assets'
-import Image from 'next/image';
+import React from "react";
+import { logo } from "@/assets";
+import Image from "next/image";
 
 export const AboutUs = () => {
   return (
     <section className="bg-white" id="about-us">
-    <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">About Us</h2>
-            <p className="mb-4 text-gray-900">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
-            <p className='text-gray-900'>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
+            About Us
+          </h2>
+          {/* <p className="mb-4 text-gray-900">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+            <p className='text-gray-900'>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p> */}
+          <p className="mb-4 text-gray-900">
+            we are dedicated to providing content producers with the
+            necessary tools to effortlessly share their ideas and stories. By
+            incorporating GitHub and utilizing dynamic content based on recent
+            contributions, we offer a personalized touch through easy
+            customization options, seamless social media integration, and a
+            user-friendly layout. </p>
+
+          <p className="text-gray-900">
+            Join us and help a worldwide audience realise the potential of
+            automated storytelling.
+          </p>
         </div>
         <div className="grid grid-cols-2 gap-4 mt-8">
-        <Image className="w-full rounded-lg" src={logo.src} width={20} height={20} alt="office content 1" />
-        <Image className="mt-4 w-full lg:mt-10 rounded-lg" src={logo.src} width={20} height={20} alt="office content 2" />
+          <Image
+            className="w-full rounded-lg"
+            src={logo.src}
+            width={20}
+            height={20}
+            alt="office content 1"
+          />
+          <Image
+            className="mt-4 w-full lg:mt-10 rounded-lg"
+            src={logo.src}
+            width={20}
+            height={20}
+            alt="office content 2"
+          />
         </div>
-    </div>
-</section>
-  )
-}
-
+      </div>
+    </section>
+  );
+};
--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -2,78 +2,157 @@ import React from 'react'
 import { logo } from '@/assets';
 import Image from 'next/image';
 
+const testimonialData = [
+    {
+        name: "Maria Smantha",
+        description: "Aperturs transformed my content sharing as a web developer. Seamless GitHub integration, dynamic posts, and customization options made it effortless and efficient. Highly recommended!",
+        position: "Web Developer",
+        imgeUrl: "https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).jpg",
+        starIcon: <ul className="mb-0 flex justify-center">
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m480 757 157 95-42-178 138-120-182-16-71-168v387ZM233 976l65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+      </ul>
+    },
+    {
+        name: "Lisa Cudrow",
+        description: "Aperturs has revolutionized content sharing for Android developers like me. With GitHub integration and dynamic post generation, it's an efficient and indispensable tool for showcasing my work. ",
+        position: "Android Developer" ,
+        imgeUrl: "https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).jpg",
+        starIcon: <ul className="mb-0 flex justify-center">
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+      </ul>
+    },
+        {
+        name: "John Smith",
+        description: "For software engineers, material sharing has been simplified by Aperturs. It is an indispensable tool for showing projects and updates because it integrates with GitHub and generates posts dynamically. Software engineers should definitely consider this!",
+        position: "Software Developer",
+        imgeUrl: "https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(3).jpg",
+        starIcon: <ul className="mb-0 flex justify-center">
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+          </svg>
+        </li>
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="w-5 text-warning">
+            <path fill="currentColor"
+              d="m323 851 157-94 157 95-42-178 138-120-182-16-71-168-71 167-182 16 138 120-42 178Zm-90 125 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-355Z" />
+          </svg>
+        </li>
+      </ul>
+    }
+]
 
 export const Testimonial = () => {
   return (
-    <section className="bg-white" id='testimonial'>
-  <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
-      <div className="mx-auto max-w-screen-sm">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold">Testimonials</h2>
-          <p className="mb-8 font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Aperturs Testimonial</p>
-      </div> 
-      <div className="grid mb-8 lg:mb-12 lg:grid-cols-2">
-          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r">
-              <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
-                  <h3 className="text-lg font-semibold text-gray-900">Speechless with how easy this was to integrate</h3>
-                  <p className="my-4">&quot;I recently got my hands on Flowbite Pro, and holy crap, I&apos;m speechless with how easy this was to integrate within my application. Most templates are a pain, code is scattered, and near impossible to theme.</p>
-                  <p className="my-4">Flowbite has code in one place and I&apos;m not joking when I say it took me a matter of minutes to copy the code, customise it and integrate within a Laravel + Vue application.</p>
-                  <p className="my-4">If you care for your time, I hands down would go with this.&quot;</p>
-              </blockquote>
-              <figcaption className="flex justify-center items-center space-x-3">
-                  <Image className="w-9 h-9 rounded-full" src={logo.src} alt="profile picture" width={10} height={10}/>
-                  <div className="space-y-0.5 font-medium text-left">
-                      <div>Aperturs</div>
-                      <div className="text-sm font-light text-gray-500 dark:text-gray-400">Aperturs Developer</div>
+<div className="container my-24 mx-auto md:px-6">
+  <section className="mb-32 text-center">
+    <h2 className="mb-12 text-3xl font-bold">Testimonials</h2>
+
+    <div className="grid gap-x-6 md:grid-cols-3 lg:gap-x-12">
+
+        {testimonialData.map((data)=>(
+                    <div className="mb-12 md:mb-0">
+                    <div className="mb-6 flex justify-center">
+                      <img src={data.imgeUrl}
+                        className="w-32 rounded-full shadow-lg dark:shadow-black/20" />
+                    </div>
+                    <h5 className="mb-2 text-lg font-bold">{data.name}</h5>
+                    <h6 className="mb-4 font-medium text-primary dark:text-primary-400">
+                      {data.position}
+                    </h6>
+                    <p className="mb-4">
+                        {data.description}
+                    {/* Quotation */}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" className="inline-block w-6">
+                        <path fill="currentColor"
+                          d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
+                      </svg>
+                    </p>
+
+                    {/* Rating */}
+                    {data.starIcon}
                   </div>
-              </figcaption>    
-          </figure>
-          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r">
-              <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
-                <h3 className="text-lg font-semibold text-gray-900">Speechless with how easy this was to integrate</h3>
-                  <p className="my-4">&quot;I recently got my hands on Flowbite Pro, and holy crap, I&apos;m speechless with how easy this was to integrate within my application. Most templates are a pain, code is scattered, and near impossible to theme.</p>
-                  <p className="my-4">Flowbite has code in one place and I&apos;m not joking when I say it took me a matter of minutes to copy the code, customise it and integrate within a Laravel + Vue application.</p>
-                  <p className="my-4">If you care for your time, I hands down would go with this.&quot;</p>
-              </blockquote>
-              <figcaption className="flex justify-center items-center space-x-3">
-                  <Image className="w-9 h-9 rounded-full" src={logo.src} alt="profile picture" width={10} height={10}/>
-                  <div className="space-y-0.5 font-medium text-left">
-                      <div>Aperturs</div>
-                      <div className="text-sm font-light text-gray-500 dark:text-gray-400">Aperturs Developer</div>
-                  </div>
-              </figcaption>    
-          </figure>
-          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r">
-              <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
-              <h3 className="text-lg font-semibold text-gray-900">Speechless with how easy this was to integrate</h3>
-                  <p className="my-4">&quot;I recently got my hands on Flowbite Pro, and holy crap, I&apos;m speechless with how easy this was to integrate within my application. Most templates are a pain, code is scattered, and near impossible to theme.</p>
-                  <p className="my-4">Flowbite has code in one place and I&apos;m not joking when I say it took me a matter of minutes to copy the code, customise it and integrate within a Laravel + Vue application.</p>
-                  <p className="my-4">If you care for your time, I hands down would go with this.&quot;</p>
-              </blockquote>
-              <figcaption className="flex justify-center items-center space-x-3">
-                  <Image className="w-9 h-9 rounded-full" src={logo.src} alt="profile picture" width={10} height={10}/>
-                  <div className="space-y-0.5 font-medium text-left">
-                      <div>Aperturs</div>
-                      <div className="text-sm font-light text-gray-500 dark:text-gray-400">Aperturs Developer</div>
-                  </div>
-              </figcaption>    
-          </figure>
-          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r">
-              <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
-              <h3 className="text-lg font-semibold text-gray-900">Speechless with how easy this was to integrate</h3>
-                  <p className="my-4">&quot;I recently got my hands on Flowbite Pro, and holy crap, I&apos;m speechless with how easy this was to integrate within my application. Most templates are a pain, code is scattered, and near impossible to theme.</p>
-                  <p className="my-4">Flowbite has code in one place and I&apos;m not joking when I say it took me a matter of minutes to copy the code, customise it and integrate within a Laravel + Vue application.</p>
-                  <p className="my-4">If you care for your time, I hands down would go with this.&quot;</p>
-              </blockquote>
-              <figcaption className="flex justify-center items-center space-x-3">
-                  <Image className="w-9 h-9 rounded-full" src={logo.src} alt="profile picture" width={10} height={10}/>
-                  <div className="space-y-0.5 font-medium text-left">
-                      <div>Aperturs</div>
-                      <div className="text-sm font-light text-gray-500 dark:text-gray-400">Aperturs Developer</div>
-                  </div>
-              </figcaption>    
-          </figure>
-      </div>
+        ))}
+
     </div>
-</section>
+  </section>
+</div>
   );
 };
